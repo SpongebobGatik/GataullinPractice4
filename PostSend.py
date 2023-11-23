eee84d49baeef259f1eda84c23e27c877728f09e
Spongebob <mistergd2281339@gmail.com>
2023-11-24 00:38:55 +0700

    Обновлено

--- PostSend.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-
-dimensions_order = input("Введите порядок измерений, разделенных пробелами (TimeInterval, URL, SourceIP): ").split()
-
-response = requests.post('http://localhost:5001/report', json={"Dimensions": dimensions_order})
-
-print(json.dumps(response.json(), indent=4, ensure_ascii=False))
